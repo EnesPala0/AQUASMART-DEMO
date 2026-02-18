09624b4700a5355199b106fca3b0d55ff5d84f33
omersoner-dev <soneromer61@gmail.com>
2026-02-19 02:23:23 +0300

    Create AquaSmart_Controller.ino

---
+++ firmware/esp32_s2/AquaSmart_Controller.ino
@@ -0,0 +1,22 @@
+/*
+  AquaSmart - Smart Water Temperature Control System
+  Board: ESP32-S2
+  IDE: Arduino IDE
+
+  Features:
+  - DS18B20 temperature sensing
+  - YF-S201 flow measurement
+  - Time-proportional valve control
+  - PI control algorithm
+  - I2C OLED interface
+  - Rotary encoder input
+  - WiFi HTTP API (demo integration)
+*/
+
+void setup() {
+  // Initialization code will be implemented here
+}
+
+void loop() {
+  // Main control loop
+}
